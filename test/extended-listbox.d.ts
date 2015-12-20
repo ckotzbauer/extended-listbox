dedcaa668ed4b1b162d42d4681c16b9fb3d3d29a
Christian <adobe94@gmx.net>
2015-12-20 14:32:32 +0100

    use TypeScript for tests

---
+++ test/extended-listbox.d.ts
@@ -0,0 +1,35 @@
+/// <reference path="../typings/jquery/jquery.d.ts"/>
+
+interface ListBoxOptions {
+    searchBar?: boolean,
+    searchBarWatermark?: string,
+    searchBarButton?: any,
+    multiple?: boolean,
+    getItems?: () => any,
+    onValueChanged?: (value: any) => void,
+    onFilterChanged?: (value: any) => void,
+    onItemsChanged?: (value: any) => void
+}
+
+interface JQuery {
+    listbox(): JQuery;
+
+    listbox(method: 'addItem', item: any): any;
+
+    listbox(method: 'removeItem', value: any): any;
+
+    listbox(method: 'destroy'): any;
+
+    listbox(method: 'getItem', value: any): any;
+
+    listbox(method: 'getItems', value: any): any;
+
+    listbox(method: 'moveItemUp', value: any): any;
+
+    listbox(method: 'moveItemDown', value: any): any;
+
+    listbox(method: 'clearSelection'): any;
+
+    //listbox(method: string): any;
+    listbox(options: ListBoxOptions): JQuery;
+}
--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,2 +1,3 @@
 
 /// <reference path="jquery/jquery.d.ts" />
+/// <reference path="qunit/qunit.d.ts" />
