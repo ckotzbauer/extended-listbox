38476071ceaafa550553b26cfd2fe49153905d76
Christian <adobe94@gmx.net>
2015-12-25 15:29:25 +0100

    new doc site

--- doc/pages/documentation/index.js
+++ doc/pages/documentation/index.js
@@ -1,27 +1,32 @@
 window.snippetMap = {};
 
-var snippets = [];
-
-snippets.push({
-    snippetId: "snippet1",
-    title: "Snippet #1",
-    subtitle: "Small Test"
-});
-
 $(function () {
-    require(["text!snippets/snippetTemplate.html"], function (snippetTemplate) {
+    require(["text!../snippetTemplate.html", "./snippets"], function (snippetTemplate, snippets) {
         // Add snippets to DOM
         var index;
-        for (index in snippets) {
-            var snippet = snippets[index];
+        for (index in snippets.snippets) {
+            var snippet = snippets.snippets[index];
             var preparedHtml = snippetTemplate
-                .replace(/SNIPPETID/g, snippet.snippetId)
-                .replace(/SUBTITLE/g, snippet.subtitle)
-                .replace(/TITLE/g, snippet.title);
+                .replace(/SNIPPETID/g, snippet.snippetId);
 
             var child = document.createElement('div');
             child.innerHTML = preparedHtml;
             child = child.firstChild;
+
+            var heading = child.children[0];
+
+            if (snippet.title) {
+                addTitle(heading, snippet.title);
+            }
+
+            if (snippet.subtitle) {
+                addSubTitle(heading, snippet.subtitle);
+            }
+
+            if (snippet.alert) {
+                addAltert(heading, snippet.alert.text, snippet.alert.type);
+            }
+
             document.getElementById('sourceSnippets').appendChild(child);
         }
 
@@ -45,6 +50,26 @@ $(function () {
         });
     });
 });
+
+function addTitle(heading, title) {
+    var titleElement = document.createElement("h3");
+    titleElement.innerHTML = title;
+    heading.appendChild(titleElement);
+}
+
+function addSubTitle(heading, subTitle) {
+    var subTitleElement = document.createElement("span");
+    subTitleElement.innerHTML = subTitle;
+    heading.appendChild(subTitleElement);
+}
+
+function addAltert(heading, text, type) {
+    var alertElement = document.createElement("div");
+    alertElement.innerHTML = text;
+    alertElement.classList.add("alert");
+    alertElement.classList.add(type);
+    heading.appendChild(alertElement);
+}
 
 function copyToClipboard(content) {
     var el = document.createElement('textarea');
