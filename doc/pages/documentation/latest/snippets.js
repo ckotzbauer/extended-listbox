3c4cefdb7a720f1035849062e0a283992f447b68
Christian <adobe94@gmx.net>
2015-12-27 14:27:29 +0100

    added snippets

--- doc/pages/documentation/latest/snippets.js
+++ doc/pages/documentation/latest/snippets.js
@@ -10,12 +10,18 @@ define("snippets", function () {
 
         this.snippets.push({
             snippetId: "snippet2",
+            title: "Multiselect list",
+            subtitle: "Multiple items are selectable."
+        });
+
+        this.snippets.push({
+            snippetId: "snippet3",
             title: "Searchable list",
             subtitle: "List with preselection and a searchBar."
         });
 
         this.snippets.push({
-            snippetId: "snippet3",
+            snippetId: "snippet4",
             title: "Grouped list items",
             subtitle: "Multiple item groups and disabled items."
         });
--- doc/pages/documentation/latest/snippets/snippet3.js
+++ doc/pages/documentation/latest/snippets/snippet3.js
@@ -2,18 +2,10 @@ $('#snippet3').listbox({
     searchBar: true,
     getItems: function() {
         return [
-            { text: "Group #1", groupHeader: true, childItems: [
-                "Item #1",
-                { text: "Item #2", disabled: true },
-                "Item #3",
-                { text: "Item #4", disabled: true }
-            ] },
-            { text: "Group #2", childItems: [
-                "Item #5",
-                { text: "Item #6", selected: true },
-                "Item #7",
-                "Item #8"
-            ] }
+            "Item #1",
+            { text: "Item #2", selected: true },
+            "Item #3",
+            "Item #4"
         ];
     }
 });
