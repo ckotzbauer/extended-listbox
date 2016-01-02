ed8315ab98da8ac3e1609e8e010ee9f308038570
Christian <adobe94@gmx.net>
2016-01-02 13:32:01 +0100

    util first version

---
+++ src/ts/Util.ts
@@ -0,0 +1,12 @@
+/// <reference path="../../typings/tsd.d.ts" />
+
+module ExtendedListbox {
+"use strict";
+
+    export class Util {
+
+        public static warning(message: string): void {
+            console.warn(message);
+        }
+    }
+}
