18ba52649b327d4a59848ac2a8737fe222209e2b
Christian <adobe94@gmx.net>
2016-01-12 19:01:43 +0100

    fixed line break

--- src/ts/infrastructure/Util.ts
+++ src/ts/infrastructure/Util.ts
@@ -8,8 +8,8 @@ module EL {
         public static deprecatedMethod(method: string, version: string, replacement: string = null): void {
             var warning: string;
             if (replacement) {
-                warning = `ExtendedListbox: Method ${method} is deprecated and
-                will be replaced with ${replacement} in ${version}.`;
+                warning = `ExtendedListbox: Method ${method} is deprecated and ` +
+                    `will be replaced with ${replacement} in ${version}.`;
             } else {
                 warning = `ExtendedListbox: Method ${method} is deprecated and will be removed in ${version}.`;
             }
