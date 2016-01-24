129aa62326e01c7992b5dca8c33c775684f959b2
Christian <adobe94@gmx.net>
2016-01-24 17:01:54 +0100

    added task

--- build/tasks/lint.js
+++ build/tasks/lint.js
@@ -2,8 +2,13 @@ var gulp = require('gulp');
 var paths = require('../paths');
 var tslint = require('gulp-tslint');
 var lesshint = require('gulp-lesshint');
+var runSequence = require("run-sequence");
 
-gulp.task('lint', function() {
+gulp.task("lint", function () {
+    return runSequence(["lint-ts", "lint-less"]);
+});
+
+gulp.task('lint-ts', function() {
     return gulp.src(paths.source)
         .pipe(tslint())
         .pipe(tslint.report('prose', {
