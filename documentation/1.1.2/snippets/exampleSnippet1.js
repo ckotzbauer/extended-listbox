68d9eca67187d21df8c4200ad0fe63db4dd642fa
Christian <adobe94@gmx.net>
2016-01-24 17:34:18 +0100

    1.1.2

---
+++ documentation/1.1.2/snippets/exampleSnippet1.js
@@ -0,0 +1,10 @@
+$('#exampleSnippet1').listbox({
+    getItems: function() {
+        return [
+            "Item #1",
+            "Item #2",
+            "Item #3",
+            "Item #4"
+        ];
+    }
+});
---
+++ documentation/1.1.2/snippets/exampleSnippet3.js
@@ -0,0 +1,11 @@
+$('#exampleSnippet3').listbox({
+    searchBar: true,
+    getItems: function() {
+        return [
+            "Item #1",
+            { text: "Item #2", selected: true },
+            "Item #3",
+            "Item #4"
+        ];
+    }
+});
