ddd952dc3bd7013972c8f99cfedf7679eed3d46d
Christian Kotzbauer <christian.kotzbauer@gmail.com>
2017-02-03 19:47:13 +0100

    remove pidfile

--- build/scripts/saucie-connect.js
+++ build/scripts/saucie-connect.js
@@ -5,8 +5,8 @@ var opts = {
     username: process.env.SAUCE_USERNAME,
     accessKey: process.env.SAUCE_ACCESS_KEY,
     verbose: true,
-    logger: console.log,
-    pidfile: pidFile
+    logger: console.log
+    //pidfile: pidFile
 };
 
 if (process.env.TRAVIS_JOB_NUMBER) {
