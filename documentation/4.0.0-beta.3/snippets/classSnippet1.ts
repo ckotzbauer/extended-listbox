2ae768de6cf8c599344f95ba6f5b94774537b950
Christian Kotzbauer <christian.kotzbauer@gmail.com>
2017-09-22 06:19:38 +0000

    Deploy to Github Pages

---
+++ documentation/4.0.0-beta.3/snippets/classSnippet1.ts
@@ -0,0 +1,28 @@
+interface ListBoxSettings {
+    /** determines if the searchBar is visible */
+    searchBar?: boolean;
+
+    /** watermark (placeholder) for the searchBar */
+    searchBarWatermark?: string;
+
+    /** settings for the searchBar button */
+    searchBarButton?: ListBoxSearchBarButtonOptions;
+
+    /** function which returns a array of items */
+    getItems?: () => (string|ListBoxItem)[];
+
+    /** callback for selection changes */
+    onValueChanged?: (event: ListBoxEvent) => void;
+
+    /** callback for searchBar text changes */
+    onFilterChanged?: (event: ListBoxEvent) => void;
+
+    /** callback for item changes (item added, item removed, item order) */
+    onItemsChanged?: (event: ListBoxEvent) => void;
+
+    /** callback for enter keyPress event on an item */
+    onItemEnterPressed?: (event: ListBoxEvent) => void;
+
+    /** callback for doubleClick event on an item */
+    onItemDoubleClicked?: (event: ListBoxEvent) => void;
+}
