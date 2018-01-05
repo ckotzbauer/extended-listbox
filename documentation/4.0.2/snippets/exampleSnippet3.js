e59bf0b1e8ed926f27f0da32a56caf02ab7887a1
Christian Kotzbauer <christian.kotzbauer@gmail.com>
2018-01-05 17:09:57 +0000

    Deploy to Github Pages

---
+++ documentation/4.0.2/snippets/exampleSnippet3.js
@@ -0,0 +1,13 @@
+var target = document.getElementById("exampleSnippet3");
+
+new SingleSelectListBox(target, {
+    searchBar: true,
+    getItems: function() {
+        return [
+            "Item #1",
+            { text: "Item #2", selected: true },
+            "Item #3",
+            "Item #4"
+        ];
+    }
+});
